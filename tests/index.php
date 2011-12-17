0a4320e4bd4f685899377182407896bf4334c5ae
Alec Elton <alec.elton@gmail.com>
2011-12-17 12:05:31 -0600

    Moved some of the setup/teardown to functions that can be called from all test cases, as that setup/teardown is common to most tests

--- tests/index.php
+++ tests/index.php
@@ -1,8 +1,71 @@
 <?php
 	require_once(dirname(__FILE__).'/simpletest/autorun.php');
+
+	$TestRoot=null;
+	$TestDataDir=null;
+
+	function removeTestData(){
+		global $TestRoot, $TestDataDir;
+		if(file_exists($TestDataDir)){
+			$dirQueue=array($TestDataDir);
+			$dirDeleteQueue=array();
+			while($dirQueue){
+				$curdir=array_shift($dirQueue);
+				$files=glob($curdir.'/*');
+				foreach($files as $file){
+					if(is_dir($file))
+						$dirQueue[]=$file;
+					else
+						unlink($file);
+				}
+				$dirDeleteQueue[]=$curdir;
+			}
+			foreach(array_reverse($dirDeleteQueue) as $dir) rmdir($dir);
+		}
+	}
+
+	function doTestSetUp(){
+		global $TestRoot, $TestDataDir;
+		$TestRoot=dirname(__FILE__).'/';
+		$TestDataDir=$TestRoot.'test-data/';
+
+		include_once $TestRoot."/../src/NanoBlog.php";
+		NanoBlog::$Root=$TestDataDir;
+
+		removeTestData();
+
+		mkdir($TestDataDir, 0770, true);
+		file_put_contents($TestDataDir.'file_test.nbpost', <<<EOT
+Title:		This is a test...
+TextTime:	Dec 13th 2011, 9:31 pm
+Author:		Alec
+Format:		raw
+
+alsdkfjaklsdjflkajsdlkfjasldkjf
+EOT
+		);
+		file_put_contents($TestDataDir.'file_test.json', <<<EOT
+{
+"Title":	"This is a test...2",
+"TextTime":	"Dec 13th 2011, 9:37 pm",
+"Author":	"Alec",
+"Format":	"raw",
+"Body":		"alsdkfjaklsdjflkajsdlkfjasldkjf"
+}
+EOT
+		);
+	}
+
+	function doTestTearDown(){
+		removeTestData();
+	}
+
 	class AllTests extends TestSuite{
 		function AllTests(){
 			parent::__construct();
 			$this->addFile(dirname(__FILE__).'/test_nanoblog.php');
+			//$this->addFile(dirname(__FILE__).'/test_zydeco_integration.php');
 		}
+
+
 	}
--- tests/test_nanoblog.php
+++ tests/test_nanoblog.php
@@ -1,64 +1,12 @@
 <?php
 	class TestNanoBlog extends UnitTestCase{
-		protected $root, $data_dir;
-
-		function removeTestData(){
-			if(file_exists($this->data_dir)){
-				$dirQueue=array($this->data_dir);
-				$dirDeleteQueue=array();
-				while($dirQueue){
-					$curdir=array_shift($dirQueue);
-					$files=glob($curdir.'/*');
-					foreach($files as $file){
-						if(is_dir($file))
-							$dirQueue[]=$file;
-						else
-							unlink($file);
-					}
-					$dirDeleteQueue[]=$curdir;
-				}
-				foreach(array_reverse($dirDeleteQueue) as $dir) rmdir($dir);
-			}
-		}
-
-		function setUp(){
-			$this->root=dirname(__FILE__).'/';
-			$this->data_dir=$this->root.'test-data/';
-
-			include_once $this->root."/../src/NanoBlog.php";
-			NanoBlog::$Root=$this->data_dir;
-
-			$this->removeTestData();
-
-			mkdir($this->data_dir, 0770, true);
-			file_put_contents($this->data_dir.'file_test.nbpost', <<<EOT
-Title:		This is a test...
-TextTime:	Dec 13th 2011, 9:31 pm
-Author:		Alec
-Format:		raw
-
-alsdkfjaklsdjflkajsdlkfjasldkjf
-EOT
-			);
-			file_put_contents($this->data_dir.'file_test.json', <<<EOT
-{
-	"Title":	"This is a test...2",
-	"TextTime":	"Dec 13th 2011, 9:37 pm",
-	"Author":	"Alec",
-	"Format":	"raw",
-	"Body":		"alsdkfjaklsdjflkajsdlkfjasldkjf"
-}
-EOT
-			);
-		}
-
-		function tearDown(){
-			$this->removeTestData();
-		}
-
+		function setUp(){ return doTestSetUp(); }
+		function tearDown(){ return doTestTearDown(); }
+		
 		function testTestEnvironment(){
-			$this->assertTrue(file_exists($this->data_dir), "Data directory exists");
-			$this->assertTrue(is_dir($this->data_dir), "Data directory is a directory");
+			global $TestRoot, $TestDataDir;
+			$this->assertTrue(file_exists($TestDataDir), "Data directory exists");
+			$this->assertTrue(is_dir($TestDataDir), "Data directory is a directory");
 		}
 
 		function testMissingFormatClass(){
@@ -68,22 +16,25 @@ EOT
 		}
 
 		function testMissingPostFile_nbpost(){
-			$file=$this->data_dir.'missing_file.nbpost';
+			global $TestRoot, $TestDataDir;
+			$file=$TestDataDir.'missing_file.nbpost';
 			$this->assertFalse(file_exists($file));
 			$this->expectException('Exception');
 			$nb=new NanoBlog('missing_file', 'nbpost');
 		}
 
 		function testMissingPostFile_json(){
-			$file=$this->data_dir.'missing_file.json';
+			global $TestRoot, $TestDataDir;
+			$file=$TestDataDir.'missing_file.json';
 			$this->assertFalse(file_exists($file));
 			$this->expectException('Exception');
 			$nb=new NanoBlog('missing_file', 'json');
 		}
 
 		function testLoadFile(){
-			$nbfile=$this->data_dir.'file_test.nbpost';
-			$jsfile=$this->data_dir.'file_test.json';
+			global $TestRoot, $TestDataDir;
+			$nbfile=$TestDataDir.'file_test.nbpost';
+			$jsfile=$TestDataDir.'file_test.json';
 			$this->assertTrue(file_exists($nbfile));
 			$this->assertTrue(file_exists($jsfile));
 			$nb_nb=new NanoBlog('file_test', 'nbpost');
